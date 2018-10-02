3556ffba3049856e2bb393752bc191230b87324f
willem1001 <wtstas@gmail.com>
2018-10-02 15:28:42 +0200

    added require

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const fs = require('fs');
+const dialogflow = require('dialogflow');
 
 const server = express();
 server.use(bodyParser.urlencoded({
