c31627608cdeb93f026b12cc74c44160402ccea2
willem1001 <wtstas@gmail.com>
2018-10-02 10:52:08 +0200

    logger

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.use(bodyParser.json());
 
 server.post('/assistant', (req, res) =>{
 
+    console.log(req.body.parameters);
     let action = req.body.queryResult && req.body.queryResult.action ;
 
     if(action === "getWeather"){
@@ -55,7 +56,7 @@ server.post('/assistant', (req, res) =>{
         let userLastName = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.last_name ? req.body.queryResult.parameters.last_name : null;
         let age = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.age ? req.body.queryResult.parameters.age : null;
         let country = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.geo_country ? req.body.queryResult.parameters.geo_country : null;
-        
+
 
     }else {
         return res.json({
