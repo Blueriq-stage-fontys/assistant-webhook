d8f47d1a316e5318dfe4f0bbca213c654297b615
willem1001 <wtstas@gmail.com>
2018-10-02 14:55:55 +0200

    logger

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var userJson;
 
 
 server.post('/assistant', (req, res) =>{
-    console.log(req.body.user.container);
+    console.log(req.body.payload.user);
     let action = req.body.queryResult && req.body.queryResult.action ;
 
     if(action === "getWeather"){
